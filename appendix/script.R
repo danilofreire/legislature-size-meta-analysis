64f193bd1145bb773763a9979bd1e85bd5269b40
Danilo Freire <danilofreire@gmail.com>
2021-09-10 02:51:24 +0200

    add descriptive stats mods, reorder table 2, dv test

--- appendix/script.R
+++ appendix/script.R
@@ -1,6 +1,6 @@
 #####################################################################################
 ## R code for "The Effect of Legislature Size on Public Spending: A Meta-Analysis" ##
-## June 2021                                                                       ##
+## September 2021                                                                  ##
 #####################################################################################
 
 ## Install and load required packages
@@ -29,7 +29,7 @@ library("compareGroups"); library(dmetar)
 
 
 ## Auxiliary functions to estimate meta-analysis models
-## and create plots
+## and create plots and tables
 
 # Broom the multilevel model
 broom_mod <- function(mod, subgroup = FALSE) {
@@ -225,6 +225,335 @@ build_forest_het <- function(dat, coef, v, slab, capt, lsize = 22, ttl = NULL, h
   return(p)
 }
 
+# Export compareGroups table to Markdown and LaTeX
+export2md2<-function(x, which.table="descr", nmax=TRUE, header.labels=c(),
+                    caption=NULL, format="html", width=Inf,
+                    strip=FALSE, first.strip=FALSE, background="#D2D2D2",
+                    size=NULL, landscape=FALSE,
+                    header.background=NULL, header.color=NULL,
+                    position="center", ...){
+
+  trim <- function(x){
+    x <- gsub("^[ ]+","",x)
+    x <- gsub("[ ]+$","",x)
+    x
+  }
+
+  prepare <- function (x, nmax, header.labels)
+    {
+
+        # x <- tab[1:3]
+        # nmax <- TRUE
+        # header.labels <- c()
+        # names(attributes(x))
+
+        show.all <- attr(x, "show.all")
+        show.descr <- attr(x, "show.descr")
+        groups <- attr(x, "groups")
+        ny <- attr(x, "ny")
+        all.last <- attr(x, "all.last")
+
+        varnames <- attr(x, "varnames")
+        nr <- attr(x, "nr")
+        desc <- x$desc
+        avail <- x$avail
+        nmax.pos <- attr(x, "nmax.pos")
+        nmax.avail.pos <- NULL
+        if (length(nmax.pos[[1]]) == 0 & length(nmax.pos[[2]]) == 0) nmax.avail.pos <- integer(0)
+        if (length(nmax.pos[[1]]) == 0 & length(nmax.pos[[2]]) > 0) nmax.avail.pos <- nmax.pos[[2]] + 1
+        if (length(nmax.pos[[1]]) > 0 & length(nmax.pos[[2]]) == 0) nmax.avail.pos <- 1
+        if (length(nmax.pos[[1]]) > 0 & length(nmax.pos[[2]]) > 0) nmax.avail.pos <- c(1, nmax.pos[[2]])
+        if (length(nmax.avail.pos) > 0 && nmax) {
+            Nmax <- apply(avail[, nmax.avail.pos, drop = FALSE],2, function(x) max(as.double(x)))
+        } else {
+            Nmax <- NULL
+            nmax <- FALSE
+        }
+
+        dd.pos <- attr(x, "dd.pos")
+        j <- 1
+        table1 <- NULL
+        if (!is.null(attr(x, "caption")))
+            cc <- character(0)
+        for (i in 1:length(varnames)) {
+            if (nr[i] == 1) {
+                t.i <- desc[j, , drop = FALSE]
+            } else {
+                t.i <- rbind(rep(NA, ncol(desc)), desc[j:(j + nr[i] -
+                                                              1), , drop = FALSE])
+                rownames(t.i)[1] <- paste(varnames[i], ":", sep = "")
+                rownames(t.i)[-1] <- sub(varnames[i], "", rownames(t.i)[-1], fixed = TRUE)
+                rownames(t.i)[-1] <- sub(": ", "    ", rownames(t.i)[-1])
+                if (length(dd.pos) < ncol(t.i)) {
+                    t.i[1, -dd.pos] <- t.i[2, -dd.pos]
+                    t.i[2, -dd.pos] <- NA
+                }
+            }
+            table1 <- rbind(table1, t.i)
+            j <- j + nr[i]
+            if (!is.null(attr(x, "caption"))) {
+                if (attr(x, "caption")[[i]] == "")
+                    cc <- c(cc, rep("", NROW(t.i)))
+                else cc <- c(cc, attr(x, "caption")[[i]], rep("",
+                                                              NROW(t.i) - 1))
+            }
+        }
+        if (ncol(table1) == 0) table1 <- table1[-1, ]
+        if (nmax) table1 <- rbind(colnames(table1), c(paste("N=", Nmax, sep = ""), rep("", ncol(table1) - length(Nmax))), table1) else table1 <- rbind(colnames(table1), table1)
+        table1 <- ifelse(is.na(table1), "", table1)
+        if (length(header.labels)==6 && is.null(names(header.labels))){
+            names(header.labels)<-c("all","p.overall","p.trend","ratio","p.ratio","N")
+        }
+        if ("all"%in%names(header.labels)){
+            ww.all<-grep("^\\[ALL\\]",trim(table1[1,]))
+            if (length(ww.all)>0){
+                ww.all<-ww.all[1]
+                table1[1,ww.all]<-header.labels["all"]
+            }
+        }
+        if ("p.overall"%in%names(header.labels)){
+            ww.p.overall<-which(table1[1,]=="p.overall")
+            if (length(ww.p.overall)>0){
+                ww.p.overall<-rev(ww.p.overall)[1]
+                table1[1,ww.p.overall]<-header.labels["p.overall"]
+            }
+        }
+        if ("p.trend"%in%names(header.labels)){
+            ww.p.trend<-which(table1[1,]=="p.trend")
+            if (length(ww.p.trend)>0){
+                ww.p.trend<-rev(ww.p.trend)[1]
+                table1[1,ww.p.trend]<-header.labels["p.trend"]
+            }
+        }
+        if ("ratio"%in%names(header.labels)){
+            ww.ratio<-which(table1[1,]%in%c("OR","HR"))
+            if (length(ww.ratio)>0){
+                ww.ratio<-rev(ww.ratio)[1]
+                table1[1,ww.ratio]<-header.labels["ratio"]
+            }
+        }
+        if ("p.ratio"%in%names(header.labels)){
+            ww.p.ratio<-which(table1[1,]=="p.ratio")
+            if (length(ww.p.ratio)>0){
+                ww.p.ratio<-rev(ww.p.ratio)[1]
+                table1[1,ww.p.ratio]<-header.labels["p.ratio"]
+            }
+        }
+        if ("N"%in%names(header.labels)){
+            ww.N<-which(table1[1,]=="N")
+            if (length(ww.N)>0){
+                ww.N<-rev(ww.N)[1]
+                table1[1,ww.N]<-header.labels["N"]
+            }
+        }
+        table1 <- apply(table1, 2, format, justify = "centre")
+        colnames(table1) <- rep("", ncol(table1))
+
+
+
+        table2 <- x[[2]]
+        table2 <- as.matrix(table2)
+        table2 <- ifelse(is.na(table2), "", table2)
+        table2 <- rbind(colnames(table2), table2)
+        table2 <- apply(table2, 2, format, justify = "centre")
+        colnames(table2) <- rep("", ncol(table2))
+
+        # rearrange table 1 and table 2 by putting all column after descriptives by groups.
+        if (all.last & show.all & show.descr & groups){
+            table1[,1:(ny+1)] <- table1[,c(2:(ny+1),1)]
+        }
+        if (all.last){
+            table2[,1:(ny+1)] <- table2[,c(2:(ny+1),1)]
+        }
+
+        # out
+        out <- list(table1 = table1, table2 = table2)
+        if (!is.null(attr(x, "caption"))) attr(out, "cc") <- cc
+        attr(out, "nmax") <- nmax
+        nr <- ifelse(nr>1, nr+1, nr)
+        nr <- cbind(nr, rep(0:1, length(nr))[1:length(nr)])
+        nr <- unlist(apply(nr, 1, function(x) rep(x[2],x[1])))
+        attr(out, "nr") <- nr
+        out
+
+    }
+
+  # compiled.format <- try(rmarkdown::all_output_formats(knitr::current_input())[1],silent=TRUE)
+  #
+  # if (inherits(compiled.format, "try-error") || is.null(compiled.format)){
+  #   warning("you are using export2md out of Rmarkdown context...")
+  # } else {
+  #   if (compiled.format%in%c("html_document","ioslides_presentation","slidy_presentation")) format <- "html"
+  #   if (compiled.format%in%c("pdf_document","beamer_presentation")) format <- "latex"
+  #   if (compiled.format=="word_document") format <- "markdown"
+  # }
+
+  if (missing(format)){
+    format <- NA
+    if (!interactive()){ # execute inside Rmarkdown
+      if (knitr::is_html_output()) format="html"
+      if (knitr::is_latex_output()) format="latex"
+      if (!knitr::is_html_output() & !knitr::is_latex_output()) format="markdown"
+      if (is.na(format)){
+        warning("Unable to identify format -> HTML assigned.")
+        format <- "html"
+      }
+    } else {# execute inside Rmarkdown
+      warning("You are calling export2md outside Rmarkdown without specifying format -> html format is assigned")
+      format <- "html"
+    }
+  }
+
+  extras <- list(...)
+  if (!inherits(x, "createTable"))
+    stop("x must be of class 'createTable'")
+  if (inherits(x, "cbind.createTable"))
+    stop("x cannot be of class 'cbind.createTable'")
+  ww <- charmatch(which.table, c("descr", "avail"))
+  if (is.na(ww))
+    stop(" argument 'which.table' must be either 'descr' or 'avail'")
+
+  if (attr(x,"groups")){
+    y.name.label<-attr(x,"yname")
+  }
+
+  if (!is.null(caption)){
+    if (!is.character(caption))
+      stop(" argument 'caption' must be a character'")
+  } else {
+    if (ww==1){
+      if (attr(x,"groups"))
+        if (inherits(x,"missingTable"))
+          caption<-paste("Missingness table by groups of `",y.name.label,"'",sep="")
+      else
+        caption<-paste("Summary descriptives table by groups of `",y.name.label,"'",sep="")
+      else
+        if (inherits(x,"missingTable"))
+          caption<-"Missingess table"
+        else
+          caption<-"Summary descriptives table"
+    }
+    if (ww==2){
+      if (attr(x,"groups"))
+        caption<-paste("Available data by groups of `",y.name.label,"'",sep="")
+      else
+        caption<-"Available data"
+    }
+  }
+  pp <- compareGroups:::prepare(x, nmax = nmax, header.labels)
+  #pp <- prepare(x, nmax = nmax, header.labels)
+  cc <- unlist(attr(pp, "cc"))
+  if (ww %in% c(1)) {
+    table1 <- pp[[1]]
+    ii <- ifelse(rownames(table1)[2] == "", 2, 1)
+    table1 <- cbind(rownames(table1), table1)
+    align <- c("l", rep("c", ncol(table1)))
+    table1[1, 1] <- " "
+    colnames(table1) <- table1[1, ]
+    colnames(table1)[-1] <- trim(colnames(table1)[-1])
+    table1 <- table1[-1, , drop = FALSE]
+    table1[,2:ncol(table1)] <- apply(table1[,-1,drop=FALSE],2,trim)
+    #table1 <- table1[-2]
+    #names(table1)[1] <- 'Extended Sample'
+    # N in the second row
+    table1 <- table1[,-3]
+    colnames(table1)[2] <- 'Extended Sample'
+    table1 <- table1[,c(1,3,2)]
+    n.exists <- nrow(table1) > 1 && length(grep("^N=", trim(table1[1, 2])))
+    if (format=="latex" & strip)
+      table1[((1+n.exists):nrow(table1)),ncol(table1)] <- ifelse(table1[((1+n.exists):nrow(table1)),ncol(table1)]=="", "\\vphantom{}", table1[((1+n.exists):nrow(table1)),ncol(table1)])
+    if (format=="latex") caption <- gsub("%","\\\\%",caption)
+    ans <- knitr::kable(table1, align = align, row.names = FALSE, caption=caption[1], format=format,
+                        booktabs=format=="latex", longtable=TRUE, linesep="", ...)
+    ans <- add_indent(ans, grep("^ ",table1[,1]))
+    if (width!=Inf) ans <- column_spec(ans, 1, width = width)
+    # groups
+    if (!is.null(cc)){
+      for (cci in 1:length(cc)){
+        if (cc[cci]!=""){
+          group.label <- cc[cci]
+          inici <- 0
+          final <- 0
+        } else {
+          if (cc[cci-1]!="")
+            group.begin <- cci-1
+          if (cci==length(cc) || cc[cci+1]!=""){
+            group.end <- cci
+            ans <- group_rows(ans, group.label, group.begin+n.exists, group.end+n.exists)
+          }
+        }
+      }
+    }
+    if (strip){
+      nr <- attr(pp, "nr")
+      ans <- row_spec(ans, which(nr==!first.strip)+n.exists, background = background)
+    }
+    if (n.exists){
+      ans <- row_spec(ans, 1, hline_after=TRUE)
+    }
+
+    if (landscape) ans <- landscape(ans)
+    if (format=="latex"){
+      ans <- kable_styling(ans, latex_options = c("repeat_header"), font_size=size, position=position)
+      #if (n.exists) ans <- gsub("\\\\midrule", "", ans) # remove lines after N
+      if (n.exists) ans <- gsub("\\\\midrule\n\\\\endfirsthead", "\\\\endfirsthead", ans) # remove lines after N
+      if (strip) ans <- gsub("\\textbackslash{}vphantom\\{\\}", "\\vphantom{}", ans, fixed=TRUE)
+    }
+    if (format=="html"){
+      ans <- kable_styling(ans, bootstrap_options=c(if (!strip) "striped" else NULL, "condensed"), full_width=FALSE, font_size=size, position=position)
+      ans <- row_spec(ans, 0, background=header.background, color=header.color)
+      ans <- row_spec(ans, if (sum(unlist(attr(x, "nmax.pos")))>0) 1 else 0, italic=sum(unlist(attr(x, "nmax.pos")))>0, extra_css = "border-bottom: 1px solid grey")
+    }
+    return(ans)
+  }
+  if (ww %in% c(2)){
+    # table2 <- compareGroups:::prepare(x, nmax = nmax, c())[[2]]
+    table2 <- prepare(x, nmax = nmax, c())[[2]]
+    table2 <- cbind(rownames(table2), table2)
+    if (!is.null(attr(x, "caption"))) {
+      cc <- unlist(attr(x, "caption"))
+      table2[, 1] <- paste("    ", table2[, 1])
+    }
+    table2[1, 1] <- " "
+    align <- c("l", rep("c", ncol(table2)))
+    colnames(table2)[-1] <- trim(table2[1, -1])
+    table2 <- table2[-1, ,drop=FALSE]
+    ans <- knitr::kable(table2, align = align, row.names = FALSE, caption=caption[1], format=format, booktabs=format=="latex", longtable=TRUE, ...)
+    # ans <- knitr::kable(table2, align = align, row.names = FALSE, caption=caption[1], format=format, booktabs=format=="latex")
+    # groups
+    if (!is.null(cc)){
+      for (cci in 1:length(cc)){
+        if (cc[cci]!=""){
+          group.label <- cc[cci]
+          inici <- 0
+          final <- 0
+        } else {
+          if (cc[cci-1]!="")
+            group.begin <- cci-1
+          if (cci==length(cc) || cc[cci+1]!=""){
+            group.end <- cci
+            ans <- group_rows(ans, group.label, group.begin, group.end)
+          }
+        }
+      }
+    }
+    ans <- add_indent(ans, integer())
+    if (strip) ans <- row_spec(ans, which(rep(0:1, nrow(table2))[1:nrow(table2)]==!first.strip), background = background)
+    if (width!=Inf) ans <- column_spec(ans, 1, width = width)
+    if (landscape) ans <- landscape(ans)
+    if (format=="latex"){
+      ans <- kable_styling(ans, latex_options = c("repeat_header"), font_size = size, position=position)
+    }
+    if (format=="html"){
+      ans <- kable_styling(ans, bootstrap_options=c(if (!strip) "striped" else NULL, "condensed"), full_width = FALSE, font_size = size, position=position)
+      ans <- row_spec(ans, 0, background=header.background, color=header.color)
+      ans <- row_spec(ans, 0, italic=FALSE, extra_css = "border-bottom: 1px solid grey")
+    }
+
+    return(ans)
+  }
+}
+
 ## Load datasets
 load("../dataset/dataCoefs.RData")
 
@@ -243,7 +572,6 @@ curve(xstar(x, beta = 0.35, theta = 0.5), from = 10, to = 20,
       main = 'Decreasing project size when varying legislature size \n (low deadweight losses)', xlab = 'Project Size', ylab = 'Number of Districts (legislature size)')
 
 ## Descriptive Statistics
-
 # Study year
 dat %>%
   select(id, year) %>%
@@ -330,26 +658,30 @@ dat %>%
 
 ## Descriptive Statistics of Moderators
 fulldat$usemeta2 <- factor(fulldat$usemeta)
-levels(fulldat$usemeta2) <- c("Extended Sample", "Main Sample")
-aux <- select(fulldat, usemeta2, indepvar2, year, published, method,
-              instdesign, elecsys2) %>%
-  mutate(indepvar2 = recode(indepvar2,
-                            `N` = "Lower Chamber Size",
-                            `K` = "Upper Chamber Size",
-                            `logN` = "Log of Lower Chamber Size"),
-         elecsys2 = recode(elecsys2,
-                           `Non-Maj` = "Non-Majoritarian",
-                           `Maj` = "Majoritarian")) %>%
+levels(fulldat$usemeta2) <- c("Other Coefficients", "Main Sample")
+aux <- select(fulldat, usemeta2, indepvar2, elecsys2, method,
+              year, published, instdesign) %>%
   rename(`Independent Variables` = indepvar2,
          `Year`                  = year,
          `Published work`        = published,
          `Estimation method`     = method,
          `Institutional Design`  = instdesign,
          `Electoral system`      = elecsys2)
-export2md(descrTable(~.-usemeta2, aux, y = aux$usemeta2,
-                        show.p.overall = F, show.all = T),
-          caption = "Descriptive Statistics of Moderators",
-          format  = "latex")
+aux$`Independent Variables` <- recode(aux$`Independent Variables`,
+                                      `N` = "Lower Chamber Size",
+                                      `K` = "Upper Chamber Size",
+                                      `logN` = "Log of Lower Chamber Size")
+aux$`Electoral system` <- recode(aux$`Electoral system`,
+                                 `Non-Maj` = "Non-Majoritarian",
+                                 `Maj` = "Majoritarian")
+aux <- select(aux, usemeta2, `Independent Variables`, Year, `Published work`, `Estimation method`, `Institutional Design`, `Electoral system`)
+aux3 <- descrTable(~.-usemeta2,
+                   aux, y = aux$usemeta2,
+                   show.p.overall = F,
+                   show.all = T)
+export2md2(aux3,
+           caption = "Descriptive Statistics of Moderators",
+           format  = "latex")
 
 ## Binomial Tests for Coefficient Signs
 
@@ -871,3 +1203,8 @@ mod <- rma.mv(yi = coef,
               sparse = TRUE,
               slab = fulldat$authoryear)
 summary(mod)
+
+## Comparing Coefficient Sizes and Dependent Variables
+aux <- dat
+aux$depvar2 <- factor(aux$depvar2, levels = c('PCTGDP', 'ExpPC', 'logExpPC'))
+summary(lm(coef~depvar2, data = aux))
